33beffe78e663e90173df564019d7619cf89dc3b
Ahsankhan556 <ak5589667@gmail.com>
2023-10-03 09:28:44 +0845

    Asimint

---
+++ app.js.js
@@ -0,0 +1,107 @@
+                                  //  ! Chapter no 26 TO 30
+                                //  ? Q NO 1
+// var now = new Date();
+// console.log(now);
+                                // ? Q NO  2
+
+// var currentDate = new Date();
+// var monthNames = [
+//   'January', 'February', 'March', 'April', 'May', 'June',
+//   'July', 'August', 'September', 'October', 'November', 'December'
+// ];
+// var currentMonth = monthNames[currentDate.getMonth()];
+// alert('The current month is: ' + currentMonth);
+
+                                //  ?Q no 3
+// var currentDate = new Date();
+// var dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+// var currentDay = dayNames[currentDate.getDay()];
+// alert('Today is: ' + currentDay);
+
+                                  //? Q no 4
+                                  
+// var currentDate = new Date();
+// var currentDay = currentDate.getDay();
+// if (currentDay === 0 || currentDay === 6) {
+//   alert("It's Fun day");
+// } else {
+//   alert("It's not Fun day");
+// }
+                                //  ? qQ no 5
+// const currentDate = new Date();
+
+// const dayOfMonth = currentDate.getDate();
+
+// if (dayOfMonth < 16) {
+// alert("First fifteen days of the month");
+// } else {
+//   alert("Last days of the month");
+// }
+                                
+                                //  ?Q no 6
+
+// const minutesSinceMidnight1970 = (function() {
+//   const currentDate = new Date();
+//   const millisecondsSince1970 = currentDate.getTime();
+//   return millisecondsSince1970 / (1000 * 60);
+// })();
+// document.write("Minutes since midnight, Jan. 1, 1970:", minutesSinceMidnight1970);
+
+                                // ?Q no 7
+
+// const currentDate = new Date();
+// const currentHour = currentDate.getHours();
+// if (currentHour < 12) {
+//   document.write("It's AM");
+// } else {
+//   document.write("It's PM");
+// }
+
+                              // ?Q no 8
+
+// var date2023 = new Date('2023-01-01');
+// date2023.setDate(date2023.getDate() - 1);
+// var laterDate = date2023;
+// document.write("Last day of December 2020:", laterDate);
+
+                            //  ? Q no 9
+
+// var ramadanStartDate = new Date('2015-06-18');
+// var currentDate = new Date();
+// var timeDifference = currentDate - ramadanStartDate;
+// var daysPassed = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+// document.write("Number of days passed since 1st Ramadan: " + daysPassed);
+
+                           // ? Q no 10
+                           
+// var referenceDate = new Date('2023-10-06'); // Change this to your desired reference date
+//  var beginningOf2015 = new Date('2015-01-01');
+// var elapsedTime = referenceDate - beginningOf2015;
+// var elapsedSeconds = elapsedTime / 1000;
+// document.write('Seconds elapsed between the reference date and the beginning of 2015: ' + elapsedSeconds + ' seconds');
+
+                            // ? Q no 11
+                            
+                            
+// var  currentDate = new Date();
+// var currentHours = currentDate.getHours();
+// currentDate.setHours(currentHours + 1);
+// document.write(currentDate);
+
+                            // ? Q no 12
+
+// var currentDate = new Date();
+// currentDate.setFullYear(currentDate.getFullYear() - 100);
+// var formattedDate = currentDate.toDateString();
+// alert(`Date 100 years ago: ${formattedDate}`);
+
+                            // ? Q no 13
+
+// var age = parseInt(prompt("Please enter your age:"));
+// var currentYear = new Date().getFullYear();
+// var birthYear = currentYear - age;
+// document.write(`Your birth year is: ${birthYear}`);
+
+                            // ? Q no 14
+
+/?????????????????????????????????????????????????????????????????????
